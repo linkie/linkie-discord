ff030c734b092d57f964d82f1cc8ae3ae0e798b3
Danielshe <shekwancheung0528@gmail.com>
2019-09-29 14:32:38 +0800

    a

--- src/main/java/me/shedaniel/linkie/CommandBase.java
+++ src/main/java/me/shedaniel/linkie/CommandBase.java
@@ -3,10 +3,12 @@ package me.shedaniel.linkie;
 import org.javacord.api.entity.message.MessageAuthor;
 import org.javacord.api.event.message.MessageCreateEvent;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ScheduledExecutorService;
 
 public interface CommandBase {
     
-    void execute(ScheduledExecutorService service, MessageCreateEvent event, MessageAuthor author, String cmd, String[] args);
+    void execute(ScheduledExecutorService service, MessageCreateEvent event, MessageAuthor author, String cmd, String[] args)
+            throws ExecutionException, InterruptedException;
     
 }
--- src/main/java/me/shedaniel/linkie/commands/FabricApiVersionCommand.java
+++ src/main/java/me/shedaniel/linkie/commands/FabricApiVersionCommand.java
@@ -3,17 +3,22 @@ package me.shedaniel.linkie.commands;
 import me.shedaniel.cursemetaapi.CurseMetaAPI;
 import me.shedaniel.linkie.CommandBase;
 import me.shedaniel.linkie.InvalidUsageException;
+import me.shedaniel.linkie.LinkieBot;
+import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageAuthor;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 
 import java.util.*;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class FabricApiVersionCommand implements CommandBase {
     @Override
-    public void execute(ScheduledExecutorService service, MessageCreateEvent event, MessageAuthor author, String cmd, String[] args) {
+    public void execute(ScheduledExecutorService service, MessageCreateEvent event, MessageAuthor author, String cmd, String[] args)
+            throws ExecutionException, InterruptedException {
         if (args.length > 1)
             throw new InvalidUsageException("+" + cmd + "[page]");
         int page = args.length == 0 ? 0 : Integer.parseInt(args[0]) - 1;
@@ -41,7 +46,52 @@ public class FabricApiVersionCommand implements CommandBase {
             CurseMetaAPI.AddonFile file = map.get(version);
             embedBuilder.addInlineField(version, file.fileName.replaceFirst("fabric-api-", "").replaceFirst("fabric-", "").replace(".jar", ""));
         });
-        embedBuilder.setDescription("Use +" + cmd + " [page] for more pages");
-        event.getChannel().sendMessage(embedBuilder).join();
+        int finalPage[] = {page};
+//        embedBuilder.setDescription("Use +" + cmd + " [page] for more pages");
+        Message message = event.getChannel().sendMessage(embedBuilder).get();
+        if (message.isServerMessage())
+            message.removeAllReactions().get();
+        message.addReactions("⬅", "❌", "➡").thenRun(() -> {
+            message.addReactionAddListener(reactionAddEvent -> {
+                try {
+                    if (!reactionAddEvent.getUser().getDiscriminatedName().equals(LinkieBot.getApi().getYourself().getDiscriminatedName()) && !reactionAddEvent.getUser().getDiscriminatedName().equals(author.getDiscriminatedName())) {
+                        reactionAddEvent.removeReaction();
+                        return;
+                    }
+                    if (reactionAddEvent.getUser().getDiscriminatedName().equals(author.getDiscriminatedName()))
+                        if (reactionAddEvent.getEmoji().equalsEmoji("❌")) {
+                            reactionAddEvent.deleteMessage();
+                        } else if (reactionAddEvent.getEmoji().equalsEmoji("⬅")) {
+                            reactionAddEvent.removeReaction();
+                            if (finalPage[0] > 0) {
+                                finalPage[0]--;
+                                List<String> versions1 = new ArrayList<>(old);
+                                versions1 = versions1.stream().skip(25 * finalPage[0]).collect(Collectors.toList());
+                                EmbedBuilder embedBuilder1 = new EmbedBuilder().setTitle("Fabric API Versions").setFooter("Page " + (finalPage[0] + 1) + "/" + ((int) Math.ceil(old.size() / 25f)) + ". Requested by " + author.getDiscriminatedName(), author.getAvatar()).setTimestampToNow();
+                                versions1.forEach(version -> {
+                                    CurseMetaAPI.AddonFile file = map.get(version);
+                                    embedBuilder.addInlineField(version, file.fileName.replaceFirst("fabric-api-", "").replaceFirst("fabric-", "").replace(".jar", ""));
+                                });
+                                message.edit(embedBuilder1);
+                            }
+                        } else if (reactionAddEvent.getEmoji().equalsEmoji("➡")) {
+                            reactionAddEvent.removeReaction();
+                            if (finalPage[0] < ((int) Math.ceil(old.size() / 25f))) {
+                                finalPage[0]++;
+                                List<String> versions1 = new ArrayList<>(old);
+                                versions1 = versions1.stream().skip(25 * finalPage[0]).collect(Collectors.toList());
+                                EmbedBuilder embedBuilder1 = new EmbedBuilder().setTitle("Fabric API Versions").setFooter("Page " + (finalPage[0] + 1) + "/" + ((int) Math.ceil(old.size() / 25f)) + ". Requested by " + author.getDiscriminatedName(), author.getAvatar()).setTimestampToNow();
+                                versions1.forEach(version -> {
+                                    CurseMetaAPI.AddonFile file = map.get(version);
+                                    embedBuilder.addInlineField(version, file.fileName.replaceFirst("fabric-api-", "").replaceFirst("fabric-", "").replace(".jar", ""));
+                                });
+                                message.edit(embedBuilder1);
+                            }
+                        }
+                } catch (Throwable throwable1) {
+                    throwable1.printStackTrace();
+                }
+            }).removeAfter(30, TimeUnit.MINUTES);
+        });
     }
 }
