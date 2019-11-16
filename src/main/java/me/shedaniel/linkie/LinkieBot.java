a0301e19bec0575d836c7accf88e34f7e84480e9
shedaniel <daniel@shedaniel.me>
2019-11-16 15:19:35 +0800

    a

--- src/main/java/me/shedaniel/linkie/LinkieBot.java
+++ src/main/java/me/shedaniel/linkie/LinkieBot.java
@@ -1,14 +1,14 @@
 package me.shedaniel.linkie;
 
-import me.shedaniel.linkie.spring.LinkieMinecraftInfoKt;
-import me.shedaniel.linkie.spring.LinkieSpringApplication;
-import org.springframework.boot.SpringApplication;
+//import me.shedaniel.linkie.spring.LinkieMinecraftInfoKt;
+//import me.shedaniel.linkie.spring.LinkieSpringApplication;
+//import org.springframework.boot.SpringApplication;
 
 public class LinkieBot {
     
     public static void main(String[] args) {
-        LinkieMinecraftInfoKt.startInfoSync();
-//        SpringApplication.run(LinkieSpringApplication.class, args);
+        //        LinkieMinecraftInfoKt.startInfoSync();
+        //        SpringApplication.run(LinkieSpringApplication.class, args);
         LinkieDiscordKt.start();
     }
     
