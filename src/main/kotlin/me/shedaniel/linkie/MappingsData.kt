72a0fdb91d67375de18f1d2035fb10f94067d8a0
shedaniel <daniel@shedaniel.me>
2019-12-29 12:58:30 +0800

    Update MappingsData.kt

--- src/main/kotlin/me/shedaniel/linkie/MappingsData.kt
+++ src/main/kotlin/me/shedaniel/linkie/MappingsData.kt
@@ -10,6 +10,6 @@ val json = Json(JsonConfiguration.Stable.copy(strictMode = false))
 private val executor = Executors.newScheduledThreadPool(16)
 
 fun startLoop() {
-//    executor.scheduleAtFixedRate(::updateYarn, 0, 20, TimeUnit.MINUTES)
+    executor.scheduleAtFixedRate(::updateYarn, 0, 20, TimeUnit.MINUTES)
     executor.scheduleAtFixedRate(::updateMCP, 0, 20, TimeUnit.MINUTES)
 }
