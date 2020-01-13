54459d705b066dd9c0459654fe7b74c3d1f4a045
shedaniel <daniel@shedaniel.me>
2020-01-13 16:09:19 +0800

    Update LinkieBot.java

--- src/main/java/me/shedaniel/linkie/LinkieBot.java
+++ src/main/java/me/shedaniel/linkie/LinkieBot.java
@@ -1,16 +1,10 @@
 package me.shedaniel.linkie;
 
-//import me.shedaniel.linkie.spring.LinkieMinecraftInfoKt;
-//import me.shedaniel.linkie.spring.LinkieSpringApplication;
-//import org.springframework.boot.SpringApplication;
-
 import me.shedaniel.linkie.audio.LinkieMusic;
 
 public class LinkieBot {
     
     public static void main(String[] args) {
-        //        LinkieMinecraftInfoKt.startInfoSync();
-        //        SpringApplication.run(LinkieSpringApplication.class, args);
         LinkieDiscordKt.start();
     }
     
