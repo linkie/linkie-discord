b7149a54847dc910306fff9b1be1004aaad340dd
shedaniel <daniel@shedaniel.me>
2020-05-11 00:47:30 +0800

    print
    Signed-off-by: shedaniel <daniel@shedaniel.me>

--- discord/src/main/kotlin/me/shedaniel/linkie/commands/QueryClassMethod.kt
+++ discord/src/main/kotlin/me/shedaniel/linkie/commands/QueryClassMethod.kt
@@ -115,6 +115,7 @@ class QueryClassMethod(private val namespace: Namespace?) : CommandBase {
                 }
             }
         } catch (t: Throwable) {
+            t.printStackTrace()
             try {
                 message.edit { it.setEmbed { it.generateThrowable(t, user) } }.subscribe()
             } catch (throwable2: Throwable) {
