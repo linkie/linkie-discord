4a4cc4ef4645a22fba14169d8f1fb591054f6e3d
shedaniel <daniel@shedaniel.me>
2020-09-16 10:04:00 +0800

    More commands and new help command
    Signed-off-by: shedaniel <daniel@shedaniel.me>

--- src/main/kotlin/me/shedaniel/linkie/discord/commands/HelpCommand.kt
+++ src/main/kotlin/me/shedaniel/linkie/discord/commands/HelpCommand.kt
@@ -8,27 +8,12 @@ import me.shedaniel.linkie.discord.*
 object HelpCommand : CommandBase {
     override fun execute(event: MessageCreateEvent, prefix: String, user: User, cmd: String, args: MutableList<String>, channel: MessageChannel) {
         args.validateEmpty(prefix, cmd)
-        val commandCategories = CommandCategory.getValues(event.guildId.orElse(null)).filter { c ->
-            CommandHandler.commands.filter { it.key.getCategory() == c && it.key.getName() != null && it.key.getDescription() != null && it.value.isNotEmpty() }.isNotEmpty()
-        }
-        commandCategories.forEachIndexed { index, category ->
-            channel.createEmbedMessage {
-                if (index == 0) setTitle("Linkie Help Command")
-                else category.description?.let(this::setTitle)
-                if (index == commandCategories.size - 1) {
-                    setFooter("Requested by " + user.discriminatedName, user.avatarUrl)
-                    setTimestampToNow()
-                }
-                CommandHandler.commands.filter { it.key.getCategory() == category && it.key.getName() != null && it.key.getDescription() != null && it.value.isNotEmpty() }
-                        .toSortedMap(compareBy { it.getName() })
-                        .forEach { (cmd, values) ->
-                            if (values.isEmpty()) return@forEach
-                            val name = cmd.getName() ?: return@forEach
-                            val desc = cmd.getDescription() ?: return@forEach
-                            addInlineField(name, "Value: ${values.joinToString { "$prefix$it" }}\n$desc")
-                        }
-            }.subscribe()
-        }
+        channel.createEmbedMessage {
+            setTitle("Linkie Help Command")
+            setFooter("Requested by " + user.discriminatedName, user.avatarUrl)
+            setTimestampToNow()
+            setDescription("View the list of commands at https://github.com/shedaniel/linkie-discord/wiki/Commands")
+        }.subscribe()
     }
 
     override fun getDescription(): String? = "Displays this message."
