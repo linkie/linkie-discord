8ebb39f666a7f49c97923b7b3c3917edeb2230fc
shedaniel <daniel@shedaniel.me>
2021-02-20 20:47:21 +0800

    Add license header
    Signed-off-by: shedaniel <daniel@shedaniel.me>

--- src/main/kotlin/me/shedaniel/linkie/discord/tricks/Trick.kt
+++ src/main/kotlin/me/shedaniel/linkie/discord/tricks/Trick.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2020 shedaniel
+ * Copyright (c) 2019, 2020, 2021 shedaniel
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
